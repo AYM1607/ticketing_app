625fe8b2031f9c1971008a879d291115b106c575
AYM1607 <u.g.a.mariano@gmail.com>
2020-09-30 22:43:36 -0500

    Redirects to landing on successfull signup. Landing page reaches the auth service to retrieve current user.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,25 @@
-const Index =  () => {
-  return <h1>Landing</h1>
-}
+import axios from "axios";
+
+const Index = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing</h1>;
+};
+
+Index.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // We're on the server.
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // We're on the browser.
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+};
 
 export default Index;
