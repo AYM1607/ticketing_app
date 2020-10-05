2cbbee74fecdb7306d66eae2c6cb738668d1332a
AYM1607 <u.g.a.mariano@gmail.com>
2020-10-05 17:01:05 -0500

    Finished first part of the client. Added the common folder to gitignore since it is an independent repository

--- client/components/header.js
+++ client/components/header.js
@@ -1,5 +1,20 @@
 import Link from "next/link";
 const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign up", href: "/auth/signup" },
+    !currentUser && { label: "Sign in", href: "/auth/signin" },
+    currentUser && { label: "Sign out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <Link href={href}>
+            <a className="nav-link">{label}</a>
+          </Link>
+        </li>
+      );
+    });
   return (
     <nav className="navbar navbar-ligh bg-light">
       <Link href="/">
@@ -7,9 +22,7 @@ const Header = ({ currentUser }) => {
       </Link>
 
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? "Sign out" : "Sign in/up"}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
